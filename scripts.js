23d832770d28dbbc386090c042b49a362514349c
brand95g <50425801+brand95g@users.noreply.github.com>
2021-04-01 21:26:29 -0400

    Refactoring
    Refactoring to remove unnecessary code

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 var form = document.querySelector("form");
 var FD = new FormData(form);
+var formDisplay = document.getElementById("myDIV");
+var successDisplay = document.getElementById("myDIV2");
 form.addEventListener( "submit", function ( event ) {
-    FD = new FormData(form);
+FD = new FormData(form);
     event.preventDefault();
-    onCaseSubmit();
+    formDisplay.style.display = "none";
+    successDisplay.style.display = "block";
     fetch("<replace with URL>", { //Replace <replace with URL> with URL of API when you want to POST to //
       method: "POST",
       mode: "no-cors",
@@ -12,27 +15,6 @@ form.addEventListener( "submit", function ( event ) {
         "Access-Control-Allow-Origin": "*",
       }
     })
-  } );
+  } ); 
 
-  function onCaseSubmit() {
-    //This hides the form post submit// 
-    var formDisplay = document.getElementById("myDIV"); //myDiv contains the form // 
-    switch(formDisplay) {
-      case "none":
-      formDisplay.style.display = "block";
-      break;
-      default:
-      formDisplay.style.display = "none";
-      break;
-    }
-//This shows the post submit message//
-    var successDisplay = document.getElementById("myDIV2"); //myDIV2 contains the post form submit message// 
-    switch(successDisplay) {
-      case "none":
-      successDisplay.style.display = "block";
-      break;
-      default:
-      successDisplay.style.display = "block";
-      break;
-    }
-  }
+
