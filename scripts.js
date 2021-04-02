f815b6f59ae3e388d6f600ca448a9468fa73cae2
brand95g <50425801+brand95g@users.noreply.github.com>
2021-04-01 21:28:08 -0400

    Formating
    Updated some formatting.

--- scripts.js
+++ scripts.js
@@ -3,18 +3,18 @@ var FD = new FormData(form);
 var formDisplay = document.getElementById("myDIV");
 var successDisplay = document.getElementById("myDIV2");
 form.addEventListener( "submit", function ( event ) {
-FD = new FormData(form);
-    event.preventDefault();
-    formDisplay.style.display = "none";
-    successDisplay.style.display = "block";
-    fetch("<replace with URL>", { //Replace <replace with URL> with URL of API when you want to POST to //
-      method: "POST",
-      mode: "no-cors",
-      body: FD,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      }
-    })
-  } ); 
+  FD = new FormData(form);
+  event.preventDefault();
+  formDisplay.style.display = "none";
+  successDisplay.style.display = "block";
+  fetch("<replace with URL>", { //Replace <replace with URL> with URL of API when you want to POST to //
+    method: "POST",
+    mode: "no-cors",
+    body: FD,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+     }
+  })
+ } ); 
 
 
